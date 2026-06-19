c9312fc7454b615d00be62affe80c6a1a605b874
sanslex <ssandor@sch.bme.hu>
2026-06-19 10:15:48 +0200

    Update pico.c
    fix error while building 'BAD_FUNC_ARG'

--- wolfcrypt/src/port/rpi_pico/pico.c
+++ wolfcrypt/src/port/rpi_pico/pico.c
@@ -26,6 +26,7 @@
 #include <string.h>
 #include <wolfssl/wolfcrypt/settings.h>
 #include <wolfssl/wolfcrypt/types.h>
+#include <wolfssl/wolfcrypt/error.h>
 
 #if defined(WOLFSSL_RPIPICO)
 #include "pico/rand.h"
